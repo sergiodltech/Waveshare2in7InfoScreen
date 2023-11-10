f2b2e29ce3395f2ca7beb76b32a8718e8b7bee47
Sergio De Leon <sergio.deleon@techoblige.com>
2023-11-10 12:05:37 +0900

    Adding emoji and weather equivalencies from wttr.in project

---
+++ resources/wttrconstants.py
@@ -0,0 +1,90 @@
+# Wttr.in project https://github.com/chubin/wttr.in/
+# WEATHER_SYMBOL modified to point to symbols Unicode equivalency
+
+WWO_CODE = {
+    "113": "Sunny",
+    "116": "PartlyCloudy",
+    "119": "Cloudy",
+    "122": "VeryCloudy",
+    "143": "Fog",
+    "176": "LightShowers",
+    "179": "LightSleetShowers",
+    "182": "LightSleet",
+    "185": "LightSleet",
+    "200": "ThunderyShowers",
+    "227": "LightSnow",
+    "230": "HeavySnow",
+    "248": "Fog",
+    "260": "Fog",
+    "263": "LightShowers",
+    "266": "LightRain",
+    "281": "LightSleet",
+    "284": "LightSleet",
+    "293": "LightRain",
+    "296": "LightRain",
+    "299": "HeavyShowers",
+    "302": "HeavyRain",
+    "305": "HeavyShowers",
+    "308": "HeavyRain",
+    "311": "LightSleet",
+    "314": "LightSleet",
+    "317": "LightSleet",
+    "320": "LightSnow",
+    "323": "LightSnowShowers",
+    "326": "LightSnowShowers",
+    "329": "HeavySnow",
+    "332": "HeavySnow",
+    "335": "HeavySnowShowers",
+    "338": "HeavySnow",
+    "350": "LightSleet",
+    "353": "LightShowers",
+    "356": "HeavyShowers",
+    "359": "HeavyRain",
+    "362": "LightSleetShowers",
+    "365": "LightSleetShowers",
+    "368": "LightSnowShowers",
+    "371": "HeavySnowShowers",
+    "374": "LightSleetShowers",
+    "377": "LightSleet",
+    "386": "ThunderyShowers",
+    "389": "ThunderyHeavyRain",
+    "392": "ThunderySnowShowers",
+    "395": "HeavySnowShowers",
+}
+
+WEATHER_SYMBOL = {
+    "Unknown":             u"2728",
+    "Cloudy":              u"2601",
+    "Fog":                 u"1f32b",
+    "HeavyRain":           u"1f327",
+    "HeavyShowers":        u"1f327",
+    "HeavySnow":           u"2744",
+    "HeavySnowShowers":    u"2744",
+    "LightRain":           u"1f326",
+    "LightShowers":        u"1f326",
+    "LightSleet":          u"1f327",
+    "LightSleetShowers":   u"1f327",
+    "LightSnow":           u"1f328",
+    "LightSnowShowers":    u"1f328",
+    "PartlyCloudy":        u"26c5",
+    "Sunny":               u"2600",
+    "ThunderyHeavyRain":   u"f1329",
+    "ThunderyShowers":     u"26c8",
+    "ThunderySnowShowers": u"26c8",
+    "VeryCloudy": u"2601",
+}
+
+# u26c5 Sun and cloudy
+# u26c8 Rain and lighthing
+# u2600 Sunny
+# u2601 Cloudy
+# u1f32b Overcast
+# u1f32c lady wind
+# u1f324 Sun with little cloud
+# u1f325 Sun with lots of cloud
+# u1f326 Sun with rainy cloud
+# u1f327 Rain
+# u1f328 Snow
+# uf1329 Lighthing
+# u2744 snowflake
+# u2728 sparkles
